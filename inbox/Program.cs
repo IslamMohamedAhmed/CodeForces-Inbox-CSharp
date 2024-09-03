57e93b35c9ce8fa09970951ec88b9f14b6463f05
IslamMohamedAhmed <islammohamedahmed49@gmail.com>
2024-09-03 09:53:03 +0300

    Inbox

---
+++ inbox/Program.cs
@@ -0,0 +1,40 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+
+int n,messages=0;
+int[] arr1 = new int[1001];
+
+n = ToInt32(ReadLine());
+string str = ReadLine();
+string[] arr2 = str.Split(" ");
+
+
+for(int i = 0; i < n; i++)
+{
+    arr1[i] = ToInt32(arr2[i]);
+}
+for (int  j= n-1;  j>=0; j--)
+{
+    if (arr1[j] == 1)
+    {
+        messages++;
+    }
+    else
+    {
+        if (messages > 0)
+        {
+            if (j != 0)
+            {
+                if (arr1[j - 1] == 1)
+                {
+                    messages ++;
+                }
+            }
+       
+        }
+    }
+}
+
+WriteLine(messages);
